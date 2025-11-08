8d25fc57a5da3a8f68dbf15a28966298c0d7f5aa
unknown <vishurudra@gmail.com>
2025-11-08 15:50:55 +0530

    second commit

---
+++ src/main/java/com/example/demo_ci_cd/DemoCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_ci_cd/DemoCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
